80cd75d79856b1895f4559a35b14c4ae1139b704
dhrumil <dhrumil.popat@myglamm.com>
2019-10-19 11:00:19 +0530

    Selection and Bubble sort added.

---
+++ bubbleSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+#include <chrono>
+using namespace std;
+using namespace std::chrono; 
+int main(){
+	int arr[] = {51,78,32,75,95,30};
+	int n = sizeof(arr)/sizeof(int);
+	auto start = high_resolution_clock::now(); 
+	for(int j=0;j<n-1;j++){
+		for(int i = 0;i<n-j-1;i++){
+			if(arr[i]>arr[i+1]){
+				int temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+			}
+		}	
+	}
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	for(int k=0;k<n;k++){
+		cout<<arr[k]<<" ";
+	}
+	cout<<"\n"<<duration.count()<<"ms\n";
+}
---
+++ selectionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+int main() {
+	auto start = high_resolution_clock::now();
+	int arr[] = {51,78,32,75,95,30};
+	int n = sizeof(arr)/sizeof(int);
+	for (int i = 0; i < n-1; i++) {
+		int iMin = i;
+		for (int j = i+1; j < n; j++){
+			if(arr[j] < arr[iMin]){
+				iMin = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[iMin];
+		arr[iMin] = temp;
+	}
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	for (int k=0;k<n;k++){
+		cout<<arr[k]<<" ";
+	}
+	cout<<"\n"<<duration.count()<<"ms\n";
+}
